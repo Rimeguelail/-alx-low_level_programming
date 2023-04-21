474ba9f8fb67a248d282d19bb9e61ad4ba5afb9d
RIMEGUELAIL <rime.guelail@yahoo.com>
2023-04-21 13:36:50 -0700

    è

---
+++ 7-print_tebahpla.c
@@ -0,0 +1,4 @@
+julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 7-print_tebahpla.c -o 7-print_tebahpla
+julien@ubuntu:~/0x01$ ./7-print_tebahpla
+zyxwvutsrqponmlkjihgfedcba
+julien@ubuntu:~/0x01$
---
+++ 8-print_base16.c
@@ -0,0 +1,4 @@
+julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-print_base16.c -o 8-print_base16
+julien@ubuntu:~/0x01$ ./8-print_base16
+0123456789abcdef
+julien@ubuntu:~/0x01$
