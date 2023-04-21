6059dbe713f828c0730b0c0e9b8da9fc72e4b3e1
RIMEGUELAIL <rime.guelail@yahoo.com>
2023-04-21 13:45:48 -0700

    print comb

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,4 @@
+julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-print_alphabets.c -o 3-print_alphabets
+julien@ubuntu:~/0x01$ ./3-print_alphabets | cat -e
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$
+julien@ubuntu:~/0x01$ 
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,4 @@
+julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-print_numbers.c -o 5-print_numbers
+julien@ubuntu:~/0x01$ ./5-print_numbers 
+0123456789
+julien@ubuntu:~/0x01$ 
