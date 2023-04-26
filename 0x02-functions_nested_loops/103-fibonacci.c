6dd388237a6b9f1863f319682b8f87ceb549ed2e
Rimeguelail <128987429+Rimeguelail@users.noreply.github.com>
2023-04-26 15:07:29 +0000

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,23 +1,51 @@
-#include <stdio.h>
-/**
- * main - main block
- * Description: computes and prints even  number < 4,000,000
- * 5 below 1024 (excluded), followed by a new line
- * Return: 0
- */
-int main(void)
-{
-	int a = 0, b = 1, next = 0;
-	int sum = 0;
+#include "main.h"
 
-	while (next < 4000000)
-	{
-		next = a + b;
-		a = b;
-		b = next;
-		if (next % 2 == 0)
-			sum += next;
-	}
-	printf("%i\n", sum);
-	return (0);
+ 
+
+/**
+* print_times_table - prints the n times table, starting with 0
+* @n: number of the times table
+*/
+void print_times_table(int n)
+{
+        int i, j, k;
+
+ 
+
+        if (n >= 0 && n <= 15)
+        {
+                for (i = 0; i <= n; i++)
+                {
+                        for (j = 0; j <= n; j++)
+                        {
+                                k = j * i;
+                                if (j == 0)
+                                {
+                                        _putchar(k + '0');
+                                } else if (k < 10 && j != 0)
+                                {
+                                        _putchar(',');
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                        _putchar(k + '0');
+                                } else if (k >= 10 && k < 100)
+                                {
+                                        _putchar(',');
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                        _putchar((k / 10) + '0');
+                                        _putchar((k % 10) + '0');
+                                } else if (k >= 100)
+                                {
+                                        _putchar(',');
+                                        _putchar(' ');
+                                        _putchar((k / 100) + '0');
+                                        _putchar(((k / 10) % 10) + '0');
+                                        _putchar((k % 10) + '0');
+                                }
+                        }
+                        _putchar('\n');
+                }
+        }
 }
